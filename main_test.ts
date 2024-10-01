df2bf3cdc47383a8673a50c8120f2639fcdf097d
Luiz Rocco <luizgustavorocco@gmail.com>
2024-10-01 01:55:28 -0300

    eliminate tests for now

--- main_test.ts
+++ main_test.ts
@@ -1,13 +1,13 @@
-import { assertEquals } from "jsr:@std/assert";
-import { Parser } from "./main.ts";
+// import { assertEquals } from "jsr:@std/assert";
+// import { Parser } from "./main.ts";
 
-Deno.test(function parserTest() {
-  const parser = new Parser();
-  const program = "1234";
+// Deno.test(function parserTest() {
+//   const parser = new Parser();
+//   const program = "1234";
 
-  const ast = parser.parser(program);
+//   const ast = parser.parser(program);
 
-  console.log(ast);
+//   console.log(ast);
 
-  assertEquals(ast, { type: "NumericLiteral", value: 1234 });
-});
+//   assertEquals(ast, { type: "NumericLiteral", value: 1234 });
+// });
